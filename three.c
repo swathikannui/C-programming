e2014b8c266a36a10aadf29f4ca0108e811e5c6c
swathikannui <38651012+swathikannui@users.noreply.github.com>
2018-08-04 06:39:17 +0000

    Add files via upload

--- three.c
+++ three.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,c
+    int a,b,c;
 
     printf("Enter three different numbers: ");
-    scanf("%d,%d,%d", &a, &b, &c);
+    scanf("%d%d%d", &a, &b, &c);
 
     if( a>=b && a>=c )
         printf("%.d is the largest number.", a);
 
-    if( b>=a && b>=c )
+    else if( b>=c )
         printf("%d is the largest number.", b);
 
-    if( c>=a && c>=b )
+    else
         printf("%d is the largest number.", c);
 
     return 0;
